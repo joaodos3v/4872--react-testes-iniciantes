11e14bd32701f7439f13af2b6502644f89896328
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-08-27 09:53:32 -0300

    video 3.4

--- src/components/FormToDo/index.spec.jsx
+++ src/components/FormToDo/index.spec.jsx
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import FormToDo from ".";
 import { TodoContext } from "../TodoProvider/TodoContext";
@@ -25,5 +26,25 @@ describe("FormToDo", () => {
 
     expect(getByRole("textbox")).toBeInTheDocument();
     expect(getByRole("textbox")).toHaveValue("Um exemplo qualquer DE NOVO");
+  });
+
+  test("deveria enviar o form com a descrição atualizada", async () => {
+    const funcaoSimulandoSubmit = jest.fn();
+    const { getByRole } = render(
+      <TodoContext.Provider
+        value={{ selectedTodo: { description: "Um exemplo qualquer DE NOVO" } }}
+      >
+        <FormToDo onSubmit={funcaoSimulandoSubmit}></FormToDo>
+      </TodoContext.Provider>,
+    );
+
+    const input = getByRole("textbox");
+    await userEvent.clear(input);
+    await userEvent.type(input, "Um novo curso para estudar");
+
+    const button = getByRole("button", { name: /salvar item/i });
+    await userEvent.click(button);
+
+    expect(funcaoSimulandoSubmit).toHaveBeenCalled();
   });
 });
