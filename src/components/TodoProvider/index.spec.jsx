c37074df4376c3b9b67c2f0f12617c230573c1cb
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-08-29 10:46:53 -0300

    video 6.2

--- src/components/TodoProvider/index.spec.jsx
+++ src/components/TodoProvider/index.spec.jsx
@@ -1,14 +1,26 @@
-import { render } from "@testing-library/react";
+import { act, render, waitFor } from "@testing-library/react";
 import React from "react";
 import { TodoProvider } from ".";
 import { getTodos } from "../../services/TodoService";
 
 jest.mock("../../services/TodoService");
 
-describe('TodoProvider', () => {
-  test('deveria renderizar o provider corretamente buscando os todos ao montar', () => {
+describe("TodoProvider", () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  test("deveria renderizar o provider corretamente buscando os todos ao montar", async () => {
     render(<TodoProvider />);
 
-    expect(getTodos).toHaveBeenCalled();
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    await waitFor(() => expect(getTodos).toHaveBeenCalled());
   });
 });
